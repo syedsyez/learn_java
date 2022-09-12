67cc0d58fb8a4ccd951076b91fb11ff04da906b6
syedsyez <110734307+syedsyez@users.noreply.github.com>
2022-09-12 08:59:09 +0530

    student dto class

---
+++ src/main/java/com/java/besant/javatr/dto/Student.java
@@ -0,0 +1,40 @@
+package com.java.besant.javatr.dto;
+
+public class Student {
+	Integer studentId;
+	String studentName;
+	int age;
+	String city;
+	String phoneNumber;
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
